ebdcc781bc3b993022a50adc4b189dbb4dbca2de
Harsh Pandey <harshpandey011@gmail.com>
2021-09-14 04:37:52 +0530

    initial commit

--- server/controllers/home.js
+++ server/controllers/home.js
@@ -1,4 +1,4 @@
 
 export const home = (req, res) => {
-    return res.status(200).json({ message: "Successfully Validated" })
+    return res.status(200).send({ message: "Successfully Validated" })
 }
