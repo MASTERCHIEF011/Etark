fa6df1c9fda6737f680b87b900d21de9f26de474
Harsh Pandey <harshpandey011@gmail.com>
2021-09-14 05:23:22 +0530

    debugging

--- server/routes/homeRoutes.js
+++ server/routes/homeRoutes.js
@@ -5,11 +5,6 @@ import auth from '../middleware/auth.js';
 
 const router = express.Router();
 
-
 router.get('/home', auth, home);
-router.get('/', (req, res) => {
-    res.send("hello uuu")
-    console.log("etarkisworkingss")
-})
 
 export default router;
